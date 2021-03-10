433a2e91500bf456ea4e24133a2ad6beca1495ec
Hewitt School Account <School@Hewitts-MacBook-Pro.local>
2021-03-09 22:30:47 -0800

    got a very basic model setup working that can be edited in the admin portal thing. No data added yet because I have a question for my model for dr. j. H HW due wed 10 is done

--- hewdit/posts/models.py
+++ hewdit/posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    body = models.CharField(max_length=2000)
+    #image = models.ImageField()
+    date = models.DateTimeField('date posted')
+    #parent = models.ForeignKey('posts.Post', on_delete=models.CASCADE)
+
+# using built in user - ask dr. j if need to add?
