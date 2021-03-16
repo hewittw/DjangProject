0d3be75f079a1b13fa026cc851e767af0fb923f5
Hewitt School Account <School@Hewitts-MacBook-Pro.local>
2021-03-15 21:43:54 -0700

    Got the database working again and changed CharField to TextField - ready to start playing around with it more

--- hewdit/posts/models.py
+++ hewdit/posts/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+# class Thread(models.Model):
+#     body = models.CharField(max_length=2000)
+#     date = models.DateTimeField('date posted')
+#     parent = models.ForeignKey('self', on_delete=models.CASCADE, default=None)
+
 class Post(models.Model):
-    body = models.CharField(max_length=2000)
+    body = models.TextField(max_length=2000) # change to text field
     #image = models.ImageField()
     date = models.DateTimeField('date posted')
-    #parent = models.ForeignKey('posts.Post', on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, default=None, null=True,
+                                blank=True)
 
 # using built in user - ask dr. j if need to add?
