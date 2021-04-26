4202274c2a75f4599fd5b680fee23d2432af4dcc
Hewitt School Account <School@Hewitts-MacBook-Pro.local>
2021-04-25 18:29:56 -0700

    Got the user logging in working. Now need to add log out functionality. Also need to decide if I use the user model or profile model throughout code because I use both right now and its causing some problems, such as with my admin user and not being able to find a profile page.

--- hewdit/posts/views.py
+++ hewdit/posts/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from .models import Post, Profile
 import datetime
+from django.template import loader
 
 # swtich to class based views
 
@@ -13,17 +14,41 @@ def index(request):
     This will the be the home page view where the user signs in. Currently
     displays little data as I have not created the forms for the user to login with.
     """
-    allPosts = Post.objects.order_by('-date')
-    print(allPosts[0].body)
-    bodys = []
-    for i in range(3):
-        bodys.append(allPosts[i].body)
-    return render(request, 'posts/baseLine.html', {'name': 'Index', 'bodys1': bodys, "allPosts": allPosts})
+    # allPosts = Post.objects.order_by('-date')
+    # print(allPosts[0].body)
+    # bodys = []
+    # for i in range(3):
+    #     bodys.append(allPosts[i].body)
+    # return render(request, 'posts/baseLine.html', {'name': 'Index', 'bodys1': bodys, "allPosts": allPosts})
+# It has forms that redirect to itself, so check if this form data is present
+    if request.POST:
+        # This tests if the form is the log *in* form
+        if 'inputUsername' in request.POST.keys():
+            # IF so, try to authentircate
+            user = authenticate(username=request.POST['inputUsername'],
+                password=request.POST['inputPassword'])
+            if user is not None:
+                # IF success, then use the login function so the session persists.
+                login(request, user)
+            else:
+                pass
+                # Message for failed login.
+        # This tests if the form is the log *out* form
+        elif 'logout' in request.POST.keys():
+            # If so, don't need to check anything else, just kill the session.
+            logout(request)
 
-# class IndexView():
-#     template_name = "posts/baseLine.html"
-#     model = Post
-#     context_object_name = "posts"
+
+    # After we chec the forms, set a flag for use in the template.
+    if request.user.is_authenticated:
+        return redirect('/posts/stream')
+    # Find the template
+    template = loader.get_template('posts/baseLine.html')
+    # The home page will show *all* posts for now.
+    context = {}
+
+    return HttpResponse(template.render(context, request))
+
 
 def stream(request):
     """
@@ -37,7 +62,7 @@ def stream(request):
                         body = request.POST['text'],
                         date = datetime.datetime.today(),
                         parent = None,
-                        userPosted = allProfiles[0].user,
+                        userPosted = request.user,
                         likes = 0)
         newPost.save()
     # get is used naturally
